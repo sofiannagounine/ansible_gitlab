5a717114e542acbe3998db6b46e365cb0d54f741
CAPFI TECH <capfitech@CAPFIGROUP.DOMAIN.com>
2017-06-23 10:14:42 +0200

    nettoyage source

--- script_post_installation.sh
+++
@@ -1,7 +0,0 @@
-mkdir /var/opt/gitlab/git-data/repositories
-
-mkdir  /var/opt/gitlab/gitlab-rails/shared/artifacts
-
-mkdir /var/opt/gitlab/gitlab-rails/shared/lfs-objects
-
-mkdir /var/opt/gitlab/gitlab-rails/shared/pages
